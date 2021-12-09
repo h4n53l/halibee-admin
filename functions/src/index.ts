dff001a58c1bf5e1cada42e7c565eb34f8f8d192
= Jonathan Hansel Awo <= jonhan17@gmail.com>
2021-12-09 07:08:19 +0000

    Added update reviews function.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,12 +5,12 @@ import firebaseAdmin = require("firebase-admin");
 firebaseAdmin.initializeApp();
 
 exports.addFreelancerRole = functions.firestore
-  .document("/newFreelancer/addNew")
-  .onUpdate((snap, context) => {
-    const userUID = snap.after.data().userUID;
-    const category = snap.after.data().category;
-    const hiveName = snap.after.data().hiveName;
-    const skill = snap.after.data().skill;
+  .document("/freelancerApplications/{userUID}")
+  .onDelete((snap, context) => {
+    const userUID = context.params.userUID;
+    const skillCategory = snap.data().skillCategory;
+    const hiveName = snap.data().hiveName;
+    const skill = snap.data().skill;
 
     firebaseAdmin
       .auth()
@@ -28,12 +28,12 @@ exports.addFreelancerRole = functions.firestore
           .firestore()
           .doc("freelancers/" + user.uid)
           .set({
+            avatar: user.photoURL,
             about: "I am a " + skill + ".",
             bannerImageURL:
-              "gs://halibee.appspot.com/images/defaultImages/banner-placeholder.png",
-            cardImageURL:
-              "gs://halibee.appspot.com/images/defaultImages/profile_placeholder.png",
-            category,
+              "https://firebasestorage.googleapis.com/v0/b/halibee.appspot.com/o/images%2FdefaultImages%2Fbanner-placeholder.png?alt=media&token=6ec79b26-fb38-4e36-8fe7-bba9de7663f5",
+            cardImageURL: user.photoURL,
+            category: skillCategory,
             description: "I am a " + skill + ".",
             displayName: user.displayName,
             hiveName,
@@ -96,3 +96,60 @@ exports.registerNewUser = functions.auth.user().onCreate((newUser) => {
       uniqueID: newUser.uid,
     });
 });
+
+exports.updateReviews = functions.database
+  .ref("{uid}/completedProjects/")
+  .onUpdate((snapshot, context) => {
+    const newlyAdded = Object.values(snapshot.after.val());
+    const totalCompletedProjects = newlyAdded.filter((project: any) =>
+    project.freelancerUID == context.params.uid
+  ).length;
+    const reviewed = newlyAdded.filter((project: any) =>
+      project.hasOwnProperty("review")
+    );
+
+    const ratings: any[] = [];
+
+    reviewed.forEach((project: any) => {
+      ratings.push(project.review.rating);
+    });
+
+    const totalReviews = ratings.length;
+    const fiveStarReviews = ratings.filter(
+      (rating: any) => rating === 5
+    ).length;
+    const fourStarReviews = ratings.filter(
+      (rating: any) => rating === 4
+    ).length;
+    const threeStarReviews = ratings.filter(
+      (rating: any) => rating === 3
+    ).length;
+    const twoStarReviews = ratings.filter((rating: any) => rating === 2).length;
+    const oneStarReviews = ratings.filter((rating: any) => rating === 1).length;
+
+
+    var sum = 0;
+    for (var i = 0; i < ratings.length; i++) {
+      sum += ratings[i];
+    }
+
+
+    var average = (sum / totalReviews).toFixed(0);
+
+    const fiveStarPercentage = ((fiveStarReviews / totalReviews) * 100).toFixed(0)+"%";
+    const fourStarPercentage = ((fourStarReviews / totalReviews) * 100).toFixed(0)+"%";
+    const threeStarPercentage = ((threeStarReviews / totalReviews) * 100).toFixed(0)+"%";
+    const twoStarPercentage = ((twoStarReviews / totalReviews) * 100).toFixed(0)+"%";
+    const oneStarPercentage = ((oneStarReviews / totalReviews) * 100).toFixed(0)+"%";
+
+    firebaseAdmin.firestore().doc("freelancers/"+context.params.uid).update({
+      projects: totalCompletedProjects,
+      rating: average,
+      totalReviews,
+      fiveStarPercentage,
+      fourStarPercentage,
+      threeStarPercentage,
+      twoStarPercentage,
+      oneStarPercentage,
+    });
+  });
